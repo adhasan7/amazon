c401a5bccadd2d59f41c33f8c99ee78e1b961cb0
adhasan7 <adsansapu@gmail.com>
2023-10-26 13:02:30 +0700

    open server env

--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,18 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const_dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
@@ -38,7 +35,6 @@ app.get("*", (req, res) =>
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
