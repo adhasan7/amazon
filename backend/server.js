52364ba591760e285526f192f186bc44dcbd75d0
adhasan7 <adsansapu@gmail.com>
2023-10-29 22:54:42 +0700

    perubahanserver1publicfrontend

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/poublic/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
